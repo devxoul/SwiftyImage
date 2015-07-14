c7189c3011b136e80d1502ebcafe14c5bc109aa1
Suyeol Jeon <devxoul@gmail.com>
2015-07-14 16:52:23 +0900

    Update playground

--- SwiftyImage.playground/Contents.swift
+++ SwiftyImage.playground/Contents.swift
@@ -1,5 +1,31 @@
-//: Playground - noun: a place where people can play
-
 import UIKit
 
-var str = "Hello, playground"
+/*:
+SwiftyImage
+===========
+
+The most sexy way to use images in Swift.
+*/
+
+//: Create images with method chaining
+//: ----------------------------------
+
+UIImage.size(width: 100, height: 100)
+       .color(UIColor.whiteColor())
+       .border(color: UIColor.redColor())
+       .border(width: 10)
+       .corner(radius: 20)
+       .image()
+
+
+//: Resizable image
+//: ---------------
+
+({ () -> UIButton in
+    let button = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 44))
+    button.setBackgroundImage(
+        UIImage.resizable().border(color: UIColor.greenColor()).border(width: 3).corner(radius: 10).image(),
+        forState: .Normal
+    )
+    return button
+})()
